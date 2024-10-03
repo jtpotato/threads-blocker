bd95195059acbd9c76eca6e7d5b4a37312e7edf7
Joel <58995538+jtpotato@users.noreply.github.com>
2024-10-03 19:01:42 +1000

    add a button

--- tb-extension/src/content.ts
+++ tb-extension/src/content.ts
@@ -1,10 +1,3 @@
-import {
-  getPostContentParent,
-  getPosts,
-  getTimeline,
-  getUsername,
-} from "./parser";
-
 let blocklist: string[] = [];
 
 // Function to fetch the blocklist
@@ -20,33 +13,69 @@ async function fetchBlocklist() {
   }
 }
 
-// Function to check posts for blocked usernames
-function checkPosts() {
-  const timeline = getTimeline();
-  if (!timeline) {
-    console.log("[Threads Blocker] No timeline found.");
-    return;
-  }
+setInterval(() => {
+  // fetch posts
+  // posts have data-pressable-container and data-interactive-id attribs.
 
-  const posts = getPosts(timeline);
-  posts.map((post) => {
-    const username = getUsername(post);
+  const posts = document.querySelectorAll(
+    'div[data-pressable-container="true"]'
+  ) as NodeListOf<HTMLDivElement>;
+
+  console.log("Scanning posts");
+
+  posts.forEach((post) => {
+    if (post.hasAttribute("data-tb-scanned")) {
+      // post has already been scanned
+      return;
+    }
+    // add data-tb-scanned attribute to post
+    post.setAttribute("data-tb-scanned", "true");
+
+    const topBar = post.childNodes[0].childNodes[1]
+      .childNodes[0] as HTMLDivElement;
+
+    if (topBar.tagName !== "DIV") {
+      // this is not a top bar.
+      return;
+    }
+
+    const username = topBar.innerText.split("\n")[0];
+
     if (blocklist.includes(username)) {
-      const contentParent = getPostContentParent(post);
-      contentParent.insertAdjacentHTML(
-        "beforebegin",
-        "<span style='color:var(--barcelona-secondary-text); font-weight:bold;'>User is on universal blocklist.</span>"
-      );
+      console.log("Found a blocked user: ", username);
+      const text = document.createElement("span");
+      text.style.color = "var(--barcelona-secondary-text)";
+      text.style.fontSize = "15px";
+      text.innerText = "(on Universal Blocklist)";
+      topBar.firstChild?.firstChild?.appendChild(text);
+    } else {
+      const button = document.createElement("button");
+      button.style.display = "inline-block";
+      button.style.marginLeft = "0.5em";
+      button.style.fontSize = "15px";
+      button.style.backgroundColor = "transparent";
+      button.style.border = "none";
+      button.style.cursor = "pointer";
+      button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg"  width="15"  height="15"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-gavel"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M13 10l7.383 7.418c.823 .82 .823 2.148 0 2.967a2.11 2.11 0 0 1 -2.976 0l-7.407 -7.385" /><path d="M6 9l4 4" /><path d="M13 10l-4 -4" /><path d="M3 21h7" /><path d="M6.793 15.793l-3.586 -3.586a1 1 0 0 1 0 -1.414l2.293 -2.293l.5 .5l3 -3l-.5 -.5l2.293 -2.293a1 1 0 0 1 1.414 0l3.586 3.586a1 1 0 0 1 0 1.414l-2.293 2.293l-.5 -.5l-3 3l.5 .5l-2.293 2.293a1 1 0 0 1 -1.414 0z" /></svg>`;
+      button.title = "Add to Universal Blocklist";
+
+      button.addEventListener("click", () => {
+        // add username to blocklist
+        fetch("https://tb-worker.jtpotatodev.workers.dev/add", {
+          method: "POST",
+          body: JSON.stringify({ username: username }),
+        });
+
+        // remove post from page
+        post.remove();
+      });
+
+      // insert as last child of the first child of the topBar
+      topBar.firstChild?.firstChild?.appendChild(button);
     }
   });
-}
+}, 500);
 
-console.log("[Threads Blocker] Active");
-
-// Initial fetch of the blocklist
 fetchBlocklist();
 
-// // Poll the timeline every 5-10 seconds
-setInterval(() => {
-  checkPosts();
-}, 5000); // Adjust time as needed (5000 ms = 5 seconds)
+console.log(blocklist);
